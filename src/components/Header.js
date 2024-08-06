cb62bddc83683185e68bf2fac91c0b937363e667
parth <parthprajapati467@gmail.com>
2024-08-06 07:54:53 -0400

    login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Use Link for navigation
-import './Header.css'; // Ensure this path is correct
+import React from "react";
+import { Link } from "react-router-dom"; // Use Link for navigation
+import "./Header.css"; // Ensure this path is correct
 // import '../components/relatio.png'
 const Header = () => {
   return (
     <header>
       <div className="container">
-         <h1>RELATIO</h1> 
+        <h1>RELATIO</h1>
         {/* <div className='img'></div> */}
         {/* <img  src='../components/relatio.png' title='relatio' alt='title' width="200" height="100"/> */}
         {/* <img src=".." alt="Alternative text" width="200" height="100" aria-label="For screen readers" /> */}
         <div className="auth-links">
-         
-          <Link to="/login">login</Link>
+          <Link to="/login">Login/Sign Up</Link>
         </div>
         <nav>
           <ul>
-            <li><Link to="/">Dashboard</Link></li>
-            <li><Link to="/Lead-Management">Lead Management</Link></li>
-            <li><Link to="/Customer-Database">Customer Database</Link></li>
-            <li><Link to="/Sales-Automation">Sales Automation</Link></li>
-            <li><Link to="/Inventory-Management">Inventory Management</Link></li>
-            <li><Link to="/Follow-Up-Reminders">Follow Up Reminders</Link></li>
-            
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/Lead-Management">Lead Management</Link>
+            </li>
+            <li>
+              <Link to="/Customer-Database">Customer Database</Link>
+            </li>
+            <li>
+              <Link to="/Sales-Automation">Sales Automation</Link>
+            </li>
+            <li>
+              <Link to="/Inventory-Management">Inventory Management</Link>
+            </li>
+            <li>
+              <Link to="/Follow-Up-Reminders">Follow Up Reminders</Link>
+            </li>
           </ul>
         </nav>
       </div>
     </header>
   );
-}
+};
 
 export default Header;
